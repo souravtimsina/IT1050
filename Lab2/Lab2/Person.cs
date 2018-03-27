63e083025dfded7409da882d82ed94fb1ac34772
Sourav Timsina <35514977+souravtimsina@users.noreply.github.com>
2018-03-26 23:07:07 -0400

    Lab2
    Lab work updated

---
+++ Lab2/Lab2/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Person
+    {
+        public String FirstName;
+        public String LastName;
+        public int Age;
+        public Person Spouse;
+
+        public static int count;
+        public static int SumOfAllAges;
+        public static double AverageAge()
+        {
+            return (double)Person.SumOfAllAges / (double)Person.Count;
+        }
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(this.GetFullName());
+            System.Console.WriteLine(this.Age);
+        }
+
+        public void AskUserForNameAndAge()
+        {
+            System.Console.Write("First Name             : ");
+            this.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Last Name              : ");
+            this.LastName = System.Console.ReadLine();
+
+            System.Console.Write("Age                    : ");
+            this.Age = int.Parse(System.Console.ReadLine());
+
+            Person.Count++;
+            Person.SumOfAllAges += this.Age;
+        }
+
+        public void CreateSpouseIfMarried()
+        {
+            System.Console.Write("Are you married? (y/n) : ");
+
+            if (System.Console.ReadLine().ToLower().StartsWith("y"))
+            {
+                this.Spouse = new Person();
+                this.Spouse.AskUserForNameAndAge();
+                this.Spouse.Spouse = this;
+            }
+        }
+    }
+}
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.AskUserForNameAndAge();
+            person1.CreateSpouseIfMarried();
+
+            Person person2 = new Person();
+            person2.AskUserForNameAndAge();
+            person2.CreateSpouseIfMarried();
+
+            Person person3 = new Person();
+            person3.AskUserForNameAndAge();
+            person3.CreateSpouseIfMarried();
+
+            System.Console.WriteLine("Average Age: " + Person.AverageAge());
+            System.Console.ReadKey();
+        }
+    }
+}
